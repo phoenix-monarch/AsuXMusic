389e0a8284ff63af5703f618688922d7361dd89b
ɴᴧʀᴜᴛᴏ ᴜᴢᴜᴍᴀᴋɪ <111524889+phoenix-monarch@users.noreply.github.com>
2023-04-02 13:57:01 +0530

    Update config.py

--- AsuXMusic/config.py
+++ AsuXMusic/config.py
@@ -8,9 +8,9 @@ admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "BQBWyIl8icee9tKdymI10oN7h8eINolbFfpUkW24IeD3QNWW17DnC46nxw4lF9eZyoJqHxzc_Z2pyfGEcz4drSy21GcGMVfY0Ri7flEMPT4G_6PynuZx6ewKyh5_UBEXxkj1P__f9owjCsDSGyXAn81cbhhVkhwr07uUqVujFTBUrcK1P8oSZp-p-ogVIveUCoQ6-Ujgos-Kkthumi-k95-cTQ4hRVAJQ41jcNFR2SSWNlnobymSFKFEKEEX-jby6C9seTOy7niaGqChY5g99fVUHdoeC-PKkbe84bIsOFWtVYeJBVmrmNiJP0wACaQLFzdE6ZM17B00mlE603JbRZDZagBYGwA")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "5841568185:AAFODqetoPeeBhBsXDyRVT7KIniZDnpWOY8")
-API_ID = int(getenv("API_ID", "20570663"))
-API_HASH = getenv("API_HASH", "209c97b1090c80f24eaf6b0ef880d088")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+API_ID = int(getenv("API_ID", "23"))
+API_HASH = getenv("API_HASH", "209c088")
 GROUP_SUPPORT = getenv("GROUP_SUPPORT", "weebs_chats")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "team_wizardz")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "1488316798").split()))
