244baff814e9871f0cb015e1cf9e050cdd92baad
ɴᴧʀᴜᴛᴏ ᴜᴢᴜᴍᴀᴋɪ <111524889+phoenix-monarch@users.noreply.github.com>
2023-04-02 13:23:49 +0530

    Update config.py

--- AsuXMusic/config.py
+++ AsuXMusic/config.py
@@ -6,15 +6,15 @@ load_dotenv()
 
 admins = {}
 
-SESSION_NAME = getenv("SESSION_NAME", "")
+SESSION_NAME = getenv("SESSION_NAME", "BQBWyIl8icee9tKdymI10oN7h8eINolbFfpUkW24IeD3QNWW17DnC46nxw4lF9eZyoJqHxzc_Z2pyfGEcz4drSy21GcGMVfY0Ri7flEMPT4G_6PynuZx6ewKyh5_UBEXxkj1P__f9owjCsDSGyXAn81cbhhVkhwr07uUqVujFTBUrcK1P8oSZp-p-ogVIveUCoQ6-Ujgos-Kkthumi-k95-cTQ4hRVAJQ41jcNFR2SSWNlnobymSFKFEKEEX-jby6C9seTOy7niaGqChY5g99fVUHdoeC-PKkbe84bIsOFWtVYeJBVmrmNiJP0wACaQLFzdE6ZM17B00mlE603JbRZDZagBYGwA")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-API_ID = int(getenv("API_ID", "8186557"))
-API_HASH = getenv("API_HASH", "efd77b34c69c164ce158037ff5a0d117")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "AbishnoiMF")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Abishnoi_bots")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5938660179").split()))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5938660179").split()))
+BOT_TOKEN = getenv("BOT_TOKEN", "5841568185:AAFODqetoPeeBhBsXDyRVT7KIniZDnpWOY8")
+API_ID = int(getenv("API_ID", "20570663"))
+API_HASH = getenv("API_HASH", "209c97b1090c80f24eaf6b0ef880d088")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "weebs_chats")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "team_wizardz")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "1488316798").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1488316798").split()))
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "120"))
 
 IMG_1 = getenv("IMG_1", "https://te.legra.ph/file/31e9ecee16a46575267a4.jpg")
